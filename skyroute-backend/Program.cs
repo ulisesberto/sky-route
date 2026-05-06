48ae1289c72e20be5311ec33f69e38e0395d09f3
ulises bertolotti <ulisesbertolotti1942@gmail.com>
2026-05-06 10:59:33 -0300

    epic fe 4 + fixes de puertos

--- skyroute-backend/Program.cs
+++ skyroute-backend/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4800")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
